42c860ca106ba1203ca726f3b30e06f3efcc7058
Alexsandr Prokhorov <kikimiki163@gmail.com>
2017-02-28 15:09:26 +0400

    DICK

---
+++ src/dick.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by aleksandr on 27.02.17.
+ */
+public class dick {
+    public static void main(String args[]){
+        Reader r = new Reader("G-гурда");
+        r.setVisible(true);
+        r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        r.setSize(200, 300);
+        r.setResizable(false);
+        r.setLocationRelativeTo(null);
+    }
+
+}
